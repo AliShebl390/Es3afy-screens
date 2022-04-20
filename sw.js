b08427299f96cd756aa1f1fc8c28931589d747a4
AhmedYasr <98847907+AhmedYasr@users.noreply.github.com>
2022-04-20 04:15:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = "Es3afy";
+var cacheName = "Es3Afy";
 var filesToCache = ["/", "/index.html", "/css/stylesheet.css", "/js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
