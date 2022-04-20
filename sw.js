92118a0e83e88d12087920e0c5f60eb89be2e49e
AhmedYasr <98847907+AhmedYasr@users.noreply.github.com>
2022-04-20 05:27:36 +0200

    .

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-var cacheName = "Es3Afy";
-var filesToCache = ["/", "/index.html", "/css/stylesheet.css", "/js/main.js"];
+// var cacheName = "Es3AFy";
+// var filesToCache = ["/", "/index.html", "/css/stylesheet.css", "/js/main.js"];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
+// /* Start the service worker and cache all of the app's content */
+// self.addEventListener("install", function (e) {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function (cache) {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+//   self.skipWaiting();
+// });
 
-/* Serve cached content when offline */
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// /* Serve cached content when offline */
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function (response) {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
