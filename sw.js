d97baf244249a7e6a46ed0ad7b11fe5ba79d31e5
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2022-04-28 03:27:53 +0200

    Add last edit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = "Es3AFy";
+var cacheName = "Es3AFyyy";
 var filesToCache = ["/", "/index.html", "/css/stylesheet.css", "/js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
